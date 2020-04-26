cdbca44ea03d86da893d10c710f7bcae510995aa
hyper5798 <hyper5798@gmail.com>
2020-04-26 21:54:49 +0800

    Add register

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,7 +12,6 @@ module.exports = {
     async index(req, res, next) {
       let token = req.body.token || decodeURI(req.query.token) || req.headers.Authorization
       
-
       try {
           let verify = await authResources.tokenVerify(token)
           if(!verify || verify.cp_id == undefined){
@@ -68,14 +67,47 @@ module.exports = {
     }
   },
 
-  async postResource(req, res, next) {
+  async register(req, res, next) {
     try {
-      let resource = {
-        firstName: "Dennis",
-        lastName: "Ritchie"
+      //Get input data
+      let token = req.body.token || decodeURI(req.query.token) || req.headers.Authorization
+      let name = req.body.name || req.query.name
+      let email = req.body.email || req.query.email
+      let password = req.body.password || req.query.password
+      let cp_id = req.body.cpId || req.query.cpId
+      //Check input data
+      if(name == undefined || email == undefined || password == undefined)
+      {
+        res.status(400).send('Miss paramater')
+        return
       }
-      resource = await asyncResources.createAysncResource(resource)
-      res.status(201).send(resource)
+      let hashCode = await authResources.getHashCode(password)
+      let myHash = hashCode.replace('$2b$', '$2y$');
+      let obj = {
+        name: name,
+        email: email,
+        password: myHash,
+        role_id: 4,
+        created_at: new Date(),
+        updated_at: new Date()
+      }
+      //For admin to add user
+      if(token !== 'undefined') {
+        let verify = await authResources.tokenVerify(token)
+        if(verify.role_id<3) {//admin
+          obj['cp_id'] = verify.cp_id
+          obj['email_verified_at'] = new Date()
+        }
+      } else {
+        if(cp_id === undefined || cp_id === null )
+        {
+          cp_id = 1
+        }
+        obj['cp_id'] = cp_id 
+      }
+      
+      let newUser = await userResources.createUser(obj)
+      res.status(200).json(newUser)
     } catch (e) {
       next(e)
     }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -28,6 +28,7 @@ const userRoute = express.Router()
 
 userRoute.get('/users', userController.index)
 userRoute.post('/users/login', userController.login)
+userRoute.post('/users/register', userController.register)
 //userRoute.put('/users/:id', userController.putResource)
 //userRoute.delete('/users/:id', userController.deleteResource)
 
