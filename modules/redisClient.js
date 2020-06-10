71003176b53ae3d71dd20e5a596a475caa116491
hyper5798 <hyper5798@gmail.com>
2020-06-10 09:10:03 +0800

    [0.2.3]Add redis prefix for laravel communication

--- modules/redisClient.js
+++ modules/redisClient.js
@@ -1,13 +1,18 @@
-var Redis = require('redis')
-var Bluebird = require('bluebird')
+const Redis = require('redis')
+const Bluebird = require('bluebird')
 Bluebird.promisifyAll(Redis.RedisClient.prototype)
 Bluebird.promisifyAll(Redis.Multi.prototype)
+/*const redis_config = {
+  "host": "127.0.0.1",
+  "port": 6379,
+  "db": 0
+};*/
 
-var client = Redis.createClient()
+let client = Redis.createClient()
 
 client
   .on('connect', function () {
-    console.log('connected')
+    console.log('Connected redis')
   })
 
 module.exports = client
