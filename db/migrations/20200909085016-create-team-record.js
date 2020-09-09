f06d4235e38218f6ccaf54fa7c48a587fc8be058
hyper5798 <hyper5798@gmail.com>
2020-09-09 17:34:32 +0800

    [0.2.61] Add team_record table and model

---
+++ db/migrations/20200909085016-create-team-record.js
@@ -0,0 +1,50 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('team_records', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      team_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      room_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      cp_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      total_time: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      total_score: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+      status: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        type: 'TIMESTAMP',
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false
+      },
+      updated_at: {
+        type: 'TIMESTAMP',
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('team_records');
+  }
+};
---
+++ db/models/team_record.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const team_record = sequelize.define('team_record', {
+    team_id: DataTypes.INTEGER,
+    room_id: DataTypes.INTEGER,
+    cp_id: DataTypes.INTEGER,
+    total_time: DataTypes.INTEGER,
+    total_score: DataTypes.INTEGER,
+    status: DataTypes.INTEGER,
+    created_at: DataTypes.DATE,
+    updated_at: DataTypes.DATE
+  }, {
+    timestamps: false
+  });
+  team_record.associate = function(models) {
+    // associations can be defined here
+  };
+  return team_record;
+};
