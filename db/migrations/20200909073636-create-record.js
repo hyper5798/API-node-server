4a9695c939b401d0cf2848f4c96d3bc47aa7e4d5
hyper5798 <hyper5798@gmail.com>
2020-09-09 17:33:37 +0800

    [0.2.60]Update record table

--- db/migrations/20200909073636-create-record.js
+++ db/migrations/20200909073636-create-record.js
@@ -9,24 +9,33 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       team_id: {
+        allowNull: false,
         type: Sequelize.INTEGER
       },
       room_id: {
+        allowNull: false,
         type: Sequelize.INTEGER
       },
       mission_id: {
+        allowNull: false,
         type: Sequelize.INTEGER
       },
       start_at: {
-        type: Sequelize.DATE
+        type: 'TIMESTAMP',
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false
       },
       end_at: {
-        type: Sequelize.DATE
+        type: 'TIMESTAMP',
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false
       },
       time: {
+        allowNull: false,
         type: Sequelize.INTEGER
       },
       score: {
+        allowNull: true,
         type: Sequelize.INTEGER
       }
     });
